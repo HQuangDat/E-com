01fd9f13aaec0da2c177ed9ecacaccf4accfb092
HoQuangDat <hoquangdat123@gmail.com>
2025-09-30 11:03:48 +0700

    feat: Implement basic e-commerce project structure

--- ecomproject/src/App.jsx
+++ ecomproject/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Homepage from './pages/Homepage.jsx'
+import Checkout from './pages/Checkout.jsx' // Import the Checkout component
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/checkout" element={<Checkout />} /> {/* Add the checkout route */}
+    </Routes>
   )
 }
 
