51ba4390eeddbe96cde41b67c5fcd3851d5fdb48
HoQuangDat <hoquangdat123@gmail.com>
2025-10-02 09:34:24 +0700

    feat: Update API endpoints in Homepage component and configure Vite server proxy

--- ecomproject/src/pages/Homepage.jsx
+++ ecomproject/src/pages/Homepage.jsx
@@ -7,10 +7,10 @@ function Homepage() {
     const [cartItems, setCartItems] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then(response => setProducts(response.data))
 
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
             .then(response => setCartItems(response.data))
     }, [])
 
